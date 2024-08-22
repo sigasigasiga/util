64c748c24a143fa7447bb002e0ad5578a4b9602c
Egor Bychin <e.siga@proton.me>
2024-08-23 00:46:10 +0400

    remove `optional.hpp` + add `brew-cross.ini` + build tests conditionally

--- src/siga/util.hpp
+++ src/siga/util.hpp
@@ -7,7 +7,6 @@
 #include <siga/util/functional.hpp>
 #include <siga/util/iterator.hpp>
 #include <siga/util/meta.hpp>
-#include <siga/util/optional.hpp>
 #include <siga/util/scoped.hpp>
 #include <siga/util/shared_from_this_base.hpp>
 #include <siga/util/storage_base.hpp>
