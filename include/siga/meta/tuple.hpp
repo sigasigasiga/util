450e2e6396490bf0e82dd7460b6860aac8ee7b15
Egor Bychin <e.siga@proton.me>
2025-01-16 18:12:46 +0400

    update `tuple_like`

--- include/siga/meta/tuple.hpp
+++ include/siga/meta/tuple.hpp
@@ -12,13 +12,15 @@ namespace siga::meta {
 // std::tuple<int &&> t = /* ... */;
 // { get<0>(t) } -> std::same_as<int &>;
 template<typename FwdTuple, std::size_t I>
-concept index_adl_gettable = requires(FwdTuple &&tuple) {
+concept tuple_index_adl_gettable = requires(FwdTuple &&tuple) {
+    requires I < std::tuple_size<std::remove_cvref_t<FwdTuple>>::value;
     get<I>(std::forward<FwdTuple>(tuple));
     typename std::tuple_element<I, std::remove_cvref_t<FwdTuple>>::type;
 };
 
 template<typename FwdTuple, std::size_t I>
-concept index_member_gettable = requires(FwdTuple &&tuple) {
+concept tuple_index_member_gettable = requires(FwdTuple &&tuple) {
+    requires I < std::tuple_size<std::remove_cvref_t<FwdTuple>>::value;
     std::forward<FwdTuple>(tuple).template get<I>();
     typename std::tuple_element<I, std::remove_cvref_t<FwdTuple>>::type;
 };
@@ -28,7 +30,7 @@ concept tuple_like =
     []<std::size_t... Is>(std::index_sequence<Is...>) constexpr {
         // note that `get<Type>(tuple)` is not always available for `std::tuple`
         // and is never available for `std::array` which is also tuple-like
-        return (... && (index_adl_gettable<T, Is> || index_member_gettable<T, Is>));
+        return (... && (tuple_index_adl_gettable<T, Is> || tuple_index_member_gettable<T, Is>));
     }(
         // compiler error messages are better if we don't use `std::tuple_size_v`
         std::make_index_sequence<std::tuple_size<std::remove_cvref_t<T>>::value>{}
