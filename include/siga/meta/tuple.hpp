797c7fa19eb2ce4fddca2a3149c1508285c3209a
Egor Bychin <e.siga@proton.me>
2025-01-29 01:44:06 +0400

    `tuple_like`: check if `get` relates to `tuple_element`

--- include/siga/meta/tuple.hpp
+++ include/siga/meta/tuple.hpp
@@ -6,24 +6,23 @@
 
 namespace siga::meta {
 
-// TODO: check that the return type relates to `std::tuple_element_t`
-//
-// the biggest problem here is references:
-// std::tuple<int &&> t = /* ... */;
-// { get<0>(t) } -> std::same_as<int &>;
+namespace impl_tuple_like {
+
 template<typename FwdTuple, std::size_t I>
-concept tuple_index_adl_gettable = requires(FwdTuple &&tuple) {
-    requires I < std::tuple_size<std::remove_cvref_t<FwdTuple>>::value;
-    get<I>(std::forward<FwdTuple>(tuple));
-    typename std::tuple_element<I, std::remove_cvref_t<FwdTuple>>::type;
+concept adl_gettable = requires(FwdTuple &&tuple) {
+    (std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&)( //
+        get<I>(std::forward<FwdTuple>(tuple))
+    );
 };
 
 template<typename FwdTuple, std::size_t I>
-concept tuple_index_member_gettable = requires(FwdTuple &&tuple) {
-    requires I < std::tuple_size<std::remove_cvref_t<FwdTuple>>::value;
-    std::forward<FwdTuple>(tuple).template get<I>();
-    typename std::tuple_element<I, std::remove_cvref_t<FwdTuple>>::type;
+concept member_gettable = requires(FwdTuple &&tuple) {
+    (std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&)( //
+        std::forward<FwdTuple>(tuple).template get<I>()
+    );
 };
+
+} // namespace impl_tuple_like
 
 template<typename T>
 concept tuple_like =
@@ -34,10 +33,10 @@ concept tuple_like =
         // 2. We can't access some elements using `t.get<I>()` and some elements using `get<I>(t)`,
         //    the type must support only one of these options for each of the fields.
         //    https://godbolt.org/z/c6TWssG14
-        if constexpr((... || tuple_index_member_gettable<T, Is>)) {
-            return (... && tuple_index_member_gettable<T, Is>);
+        if constexpr((... || impl_tuple_like::member_gettable<T, Is>)) {
+            return (... && impl_tuple_like::member_gettable<T, Is>);
         } else {
-            return (... && tuple_index_adl_gettable<T, Is>);
+            return (... && impl_tuple_like::adl_gettable<T, Is>);
         }
     }(
         // compiler error messages are better if we don't use `std::tuple_size_v`
--- test/tuple.cpp
+++ test/tuple.cpp
@@ -3,40 +3,202 @@
 
 #include <siga/meta/tuple.hpp>
 
-namespace test {
-
-struct foo
+struct member_get
 {
+public:
     template<std::size_t I>
-    requires(I == 1)
     int get()
     {
-        return 0;
+        return I;
     }
 };
 
-template<std::size_t I>
-requires(I == 0)
-int get(foo)
-{
-    return 1;
-}
-
-} // namespace test
-
-template<std::size_t I>
-class std::tuple_element<I, test::foo> : public std::type_identity<int>
+template<>
+struct std::tuple_size<member_get> : std::integral_constant<std::size_t, 1>
 {};
 
 template<>
-class std::tuple_size<test::foo> : public std::integral_constant<std::size_t, 2>
+struct std::tuple_element<0, member_get> : std::type_identity<int>
 {};
+
+// -------------------------------------------------------------------------------------------------
+
+struct mismatching_types
+{
+public:
+    template<std::size_t I>
+    const char *get()
+    {
+        return "";
+    }
+};
+
+template<>
+struct std::tuple_size<mismatching_types> : std::integral_constant<std::size_t, 1>
+{};
+
+template<>
+struct std::tuple_element<0, mismatching_types> : std::type_identity<std::string>
+{};
+
+// -------------------------------------------------------------------------------------------------
+
+struct explicit_constructor
+{
+    explicit explicit_constructor(int) {}
+};
+
+struct mismatching_and_explicit
+{
+    template<std::size_t I>
+    int get()
+    {
+        return I;
+    }
+};
+
+template<>
+struct std::tuple_size<mismatching_and_explicit> : std::integral_constant<std::size_t, 1>
+{};
+
+template<>
+struct std::tuple_element<0, mismatching_and_explicit> : std::type_identity<explicit_constructor>
+{};
+
+// -------------------------------------------------------------------------------------------------
+
+struct adl_get_is_a_friend
+{
+    template<std::size_t I>
+    friend int get(adl_get_is_a_friend)
+    {
+        return I;
+    }
+};
+
+template<>
+struct std::tuple_size<adl_get_is_a_friend> : std::integral_constant<std::size_t, 1>
+{};
+
+template<>
+struct std::tuple_element<0, adl_get_is_a_friend> : std::type_identity<int>
+{};
+
+// -------------------------------------------------------------------------------------------------
+
+struct some_with_member_and_some_with_adl
+{
+    template<std::size_t I>
+    requires(I == 0)
+    int get()
+    {
+        return I;
+    }
+
+    template<std::size_t I>
+    requires(I == 1)
+    friend int get(some_with_member_and_some_with_adl)
+    {
+        return I;
+    }
+};
+
+template<>
+struct std::tuple_size<some_with_member_and_some_with_adl> : std::integral_constant<std::size_t, 2>
+{};
+
+template<std::size_t I>
+struct std::tuple_element<I, some_with_member_and_some_with_adl> : std::type_identity<int>
+{};
+
+// -------------------------------------------------------------------------------------------------
+
+struct some_with_member_and_some_with_adl_2
+{
+    // a function that'd never be actually used because `tuple_size == 2`
+    template<std::size_t I>
+    requires(I > 100)
+    int get()
+    {
+        return I;
+    }
+
+    template<std::size_t I>
+    friend int get(some_with_member_and_some_with_adl_2)
+    {
+        return I;
+    }
+};
+
+template<>
+struct std::tuple_size<some_with_member_and_some_with_adl_2>
+    : std::integral_constant<std::size_t, 2>
+{};
+
+template<std::size_t I>
+struct std::tuple_element<I, some_with_member_and_some_with_adl_2> : std::type_identity<int>
+{};
+
+// -------------------------------------------------------------------------------------------------
+
+struct type_is_a_ref_but_get_returns_a_value
+{
+public:
+    template<std::size_t I>
+    int get()
+    {
+        return I;
+    }
+};
+
+template<>
+struct std::tuple_size<type_is_a_ref_but_get_returns_a_value>
+    : std::integral_constant<std::size_t, 1>
+{};
+
+template<>
+struct std::tuple_element<0, type_is_a_ref_but_get_returns_a_value> : std::type_identity<int &>
+{};
+
+// -------------------------------------------------------------------------------------------------
 
 int main()
 {
-    static_assert(!siga::meta::tuple_like<test::foo>);
-    static_assert(siga::meta::tuple_like<std::tuple<>>);
-    static_assert(siga::meta::tuple_like<std::tuple<int, int>>);
-    static_assert(siga::meta::tuple_like<std::tuple<int &, int &&>>);
-    static_assert(siga::meta::tuple_like<std::ranges::subrange<int *, void *>>);
+    using namespace siga::meta;
+
+    auto &&[begin, end] = std::ranges::subrange((int *){}, (void *){});
+
+    // `get` returns a value
+    static_assert(tuple_like<std::ranges::subrange<int *, void *>>);
+    static_assert(tuple_like<std::ranges::subrange<int *, void *> &>);
+    static_assert(tuple_like<std::ranges::subrange<int *, void *> &&>);
+
+    // `get` returns a reference
+    static_assert(tuple_like<std::tuple<>>);
+    static_assert(tuple_like<std::tuple<int &>>);
+    static_assert(tuple_like<std::tuple<int &&>>);
+    static_assert(tuple_like<std::tuple<int &> &>);
+    static_assert(tuple_like<std::tuple<int &&> &>);
+
+    // `get` is a member function
+    static_assert(tuple_like<member_get>);
+
+    // `get` returns type that is convertible to `tuple_element_t`
+    static_assert(tuple_like<mismatching_types>);
+
+    // `get` is a friend function defined in the class
+    static_assert(tuple_like<adl_get_is_a_friend>);
+
+    // self-explanatory
+    static_assert(not tuple_like<some_with_member_and_some_with_adl>);
+
+    // FIXME? it works but probably shouldn't because structured bindings don't work with it
+    // though there's probably no way it can be fixed
+    static_assert(tuple_like<some_with_member_and_some_with_adl_2>);
+
+    // `get` returns a type that is only _explicitly_ convertible to `tuple_element_t`
+    static_assert(not tuple_like<mismatching_and_explicit>);
+
+    // self-explanatory
+    static_assert(not tuple_like<type_is_a_ref_but_get_returns_a_value>);
 }
