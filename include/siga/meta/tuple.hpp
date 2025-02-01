628afa4d235994a314ef630dc49d72a22d57975e
Egor Bychin <e.siga@proton.me>
2025-02-01 22:29:18 +0400

    do not use c-style cast in `tuple_like`

--- include/siga/meta/tuple.hpp
+++ include/siga/meta/tuple.hpp
@@ -6,23 +6,23 @@
 
 namespace siga::meta {
 
-namespace impl_tuple_like {
-
-template<typename FwdTuple, std::size_t I>
-concept adl_gettable = requires(FwdTuple &&tuple) {
-    (std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&)( //
-        get<I>(std::forward<FwdTuple>(tuple))
-    );
-};
+namespace detail_tuple_like {
 
 template<typename FwdTuple, std::size_t I>
 concept member_gettable = requires(FwdTuple &&tuple) {
-    (std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&)( //
+    {
         std::forward<FwdTuple>(tuple).template get<I>()
-    );
+    } -> std::convertible_to<std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&>;
 };
 
-} // namespace impl_tuple_like
+template<typename FwdTuple, std::size_t I>
+concept adl_gettable = requires(FwdTuple &&tuple) {
+    {
+        get<I>(std::forward<FwdTuple>(tuple))
+    } -> std::convertible_to<std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&>;
+};
+
+} // namespace detail_tuple_like
 
 template<typename T>
 concept tuple_like =
@@ -33,10 +33,10 @@ concept tuple_like =
         // 2. We can't access some elements using `t.get<I>()` and some elements using `get<I>(t)`,
         //    the type must support only one of these options for each of the fields.
         //    https://godbolt.org/z/c6TWssG14
-        if constexpr((... || impl_tuple_like::member_gettable<T, Is>)) {
-            return (... && impl_tuple_like::member_gettable<T, Is>);
+        if constexpr((... || detail_tuple_like::member_gettable<T, Is>)) {
+            return (... && detail_tuple_like::member_gettable<T, Is>);
         } else {
-            return (... && impl_tuple_like::adl_gettable<T, Is>);
+            return (... && detail_tuple_like::adl_gettable<T, Is>);
         }
     }(
         // compiler error messages are better if we don't use `std::tuple_size_v`
