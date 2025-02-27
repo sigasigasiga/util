a62b2767d94f2d38c781c1a891c5696d6fde67a3
Egor Bychin <e.siga@proton.me>
2025-02-27 15:27:50 +0400

    make `tuple_like` more SFINAE-friendly

--- include/siga/meta/tuple.hpp
+++ include/siga/meta/tuple.hpp
@@ -8,18 +8,22 @@ namespace siga::meta {
 
 namespace detail_tuple_like {
 
-template<typename FwdTuple, std::size_t I>
-concept member_gettable = requires(FwdTuple &&tuple) {
-    {
-        std::forward<FwdTuple>(tuple).template get<I>()
-    } -> std::convertible_to<std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&>;
+// clang-format off
+template<typename T, std::size_t I, typename FwdTuple>
+concept convertible_to_tuple_element = std::convertible_to<
+    T,
+    typename std::tuple_element<I, std::remove_reference_t<FwdTuple>>::type &&
+>;
+// clang-format on
+
+template<std::size_t I, typename FwdTuple>
+concept member_gettable = requires(FwdTuple &&tup) {
+    { std::forward<FwdTuple>(tup).template get<I>() } -> convertible_to_tuple_element<I, FwdTuple>;
 };
 
-template<typename FwdTuple, std::size_t I>
-concept adl_gettable = requires(FwdTuple &&tuple) {
-    {
-        get<I>(std::forward<FwdTuple>(tuple))
-    } -> std::convertible_to<std::tuple_element_t<I, std::remove_reference_t<FwdTuple>> &&>;
+template<std::size_t I, typename FwdTuple>
+concept adl_gettable = requires(FwdTuple &&tup) {
+    { get<I>(std::forward<FwdTuple>(tup)) } -> convertible_to_tuple_element<I, FwdTuple>;
 };
 
 } // namespace detail_tuple_like
@@ -33,10 +37,10 @@ concept tuple_like =
         // 2. We can't access some elements using `t.get<I>()` and some elements using `get<I>(t)`,
         //    the type must support only one of these options for each of the fields.
         //    https://godbolt.org/z/c6TWssG14
-        if constexpr((... || detail_tuple_like::member_gettable<T, Is>)) {
-            return (... && detail_tuple_like::member_gettable<T, Is>);
+        if constexpr((... || detail_tuple_like::member_gettable<Is, T>)) {
+            return (... && detail_tuple_like::member_gettable<Is, T>);
         } else {
-            return (... && detail_tuple_like::adl_gettable<T, Is>);
+            return (... && detail_tuple_like::adl_gettable<Is, T>);
         }
     }(
         // compiler error messages are better if we don't use `std::tuple_size_v`
