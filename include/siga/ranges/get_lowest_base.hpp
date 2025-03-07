120ed73d12862002dae038ce138e87e013de592e
Egor Bychin <e.siga@proton.me>
2025-03-08 04:03:26 +0500

    make `get_lowest_base` sfinae-friendly

--- include/siga/ranges/get_lowest_base.hpp
+++ include/siga/ranges/get_lowest_base.hpp
@@ -6,18 +6,38 @@
 
 namespace siga::ranges {
 
-template<std::ranges::range Range>
-[[nodiscard]] constexpr decltype(auto) get_lowest_base(Range &&range)
-{
-    constexpr bool has_base = requires {
-        { std::forward<Range>(range).base() } -> std::ranges::range;
-    };
+// clang-format off
 
-    if constexpr(has_base) {
-        return (get_lowest_base)(std::forward<Range>(range).base());
-    } else {
-        return util::cut_rvalue_ref(std::forward<Range>(range));
-    }
+namespace detail_get_lowest_base {
+
+class adl_tag{};
+
+template<typename Range>
+[[nodiscard]] constexpr auto impl(adl_tag, Range &&range, util::overload_priority<0>)
+    noexcept(noexcept(util::cut_rvalue_ref(std::forward<Range>(range))))
+    -> decltype(util::cut_rvalue_ref(std::forward<Range>(range)))
+{
+    return util::cut_rvalue_ref(std::forward<Range>(range));
 }
+
+template<typename Range>
+[[nodiscard]] constexpr auto impl(adl_tag, Range &&range, util::overload_priority<1> = {})
+    noexcept(noexcept(impl(adl_tag{}, std::forward<Range>(range).base(), util::overload_priority<1>{})))
+    -> decltype(impl(adl_tag{}, std::forward<Range>(range).base(), util::overload_priority<1>{}))
+{
+    return impl(adl_tag{}, std::forward<Range>(range).base(), util::overload_priority<1>{});
+}
+
+} // namespace detail_get_lowest_base
+
+template<std::ranges::range Range>
+[[nodiscard]] constexpr auto get_lowest_base(Range &&range)
+    noexcept(noexcept(impl(detail_get_lowest_base::adl_tag{}, std::forward<Range>(range))))
+    -> decltype(impl(detail_get_lowest_base::adl_tag{}, std::forward<Range>(range)))
+{
+    return impl(detail_get_lowest_base::adl_tag{}, std::forward<Range>(range));
+}
+
+// clang-format on
 
 } // namespace siga::ranges
