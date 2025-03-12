b92b151832ff7ec314793aaf42a6424d75a021f7
Egor Bychin <e.siga@proton.me>
2025-03-12 17:14:34 +0400

    add `implicit_cast`

--- include/siga/util/utility.hpp
+++ include/siga/util/utility.hpp
@@ -109,4 +109,16 @@ requires std::is_reference_v<CvRefTo> &&
 
 // clang-format on
 
+// -------------------------------------------------------------------------------------------------
+
+// 1. I guess there's no way to implement it by writing it three times
+// 2. If `To` is `void`, then it is a SFINAE-out case. I'm not sure if this is desired
+template<typename To, typename From>
+requires std::is_convertible_v<From, To>
+[[nodiscard]] constexpr To implicit_cast(From &&from)
+    noexcept(std::is_nothrow_convertible_v<From, To>)
+{
+    return from;
+}
+
 } // namespace siga::util
