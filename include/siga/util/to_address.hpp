6936c4fe90245da437c4614da1b3692653866577
Egor Bychin <e.siga@proton.me>
2025-03-17 20:25:48 +0400

    Revert "`to_address` -> `to_address` + `to_address_arr`"
    This reverts commit 891ff82e91e8274dbd4396236e2ed8b4d6a2d38f.

--- include/siga/util/to_address.hpp
+++ include/siga/util/to_address.hpp
@@ -1,49 +1,21 @@
 #pragma once
 
+#include <concepts>
 #include <memory>
+
+#include <siga/util/utility.hpp>
 
 namespace siga::util {
 
-// like `std::to_address` but SFINAE-friendly
+namespace detail_to_address {
 
-template<typename T>
-[[nodiscard]] constexpr T *to_address(T *ptr) noexcept
-{
-    static_assert(!std::is_function_v<T>);
-    return ptr;
-}
+constexpr overload_priority<3> highest_priority;
+
+class adl_tag
+{};
 
 template<typename Ptr>
-[[nodiscard]] constexpr auto to_address(const Ptr &ptr, ...)
-    noexcept(noexcept((to_address)(ptr.operator->()))) //
-    -> decltype((to_address)(ptr.operator->()))
-{
-    // drill down behavior
-    return (to_address)(ptr.operator->());
-}
-
-// STL doesn't check that the type is a pointer, so do I because the only thing I want is SFINAE
-template<typename Ptr>
-[[nodiscard]] constexpr auto to_address(const Ptr &ptr, int = 0)
-    noexcept(noexcept(std::pointer_traits<Ptr>::to_address(ptr))) //
-    -> decltype(std::pointer_traits<Ptr>::to_address(ptr))
-{
-    return std::pointer_traits<Ptr>::to_address(ptr);
-}
-
-// =================================================================================================
-
-// like `siga::util::to_address` but supports `std::{unique,shared}_ptr<T[]>`
-template<typename Ptr>
-[[nodiscard]] constexpr auto to_address_arr(const Ptr &ptr, int = 0)
-    noexcept(noexcept((to_address)(ptr))) //
-    -> decltype((to_address)(ptr))
-{
-    return (to_address)(ptr);
-}
-
-template<typename Ptr>
-[[nodiscard]] constexpr auto to_address_arr(const Ptr &ptr, ...)
+constexpr auto impl(adl_tag, const Ptr &ptr, overload_priority<0>) noexcept(noexcept(ptr.get()))
 requires requires(std::size_t i) {
     // overload for `unique_ptr<T[]>` and `shared_ptr<T[]>`.
     // it isn't perfect by any means but i guess that's something?
@@ -53,5 +25,43 @@ requires requires(std::size_t i) {
 {
     return ptr.get();
 }
+
+template<typename Ptr>
+constexpr auto impl(adl_tag, const Ptr &ptr, overload_priority<1>)
+    noexcept(noexcept(impl(adl_tag{}, ptr.operator->(), highest_priority))) //
+    -> decltype(impl(adl_tag{}, ptr.operator->(), highest_priority))
+{
+    return impl(adl_tag{}, ptr.operator->(), highest_priority);
+}
+
+// clang-format off
+template<typename Ptr>
+constexpr meta::conceptify<std::is_pointer> auto impl(adl_tag, const Ptr &ptr, overload_priority<2>)
+    noexcept(noexcept(std::pointer_traits<Ptr>::to_address(ptr)))
+    requires requires { std::pointer_traits<Ptr>::to_address(ptr); }
+{
+    return std::pointer_traits<Ptr>::to_address(ptr);
+}
+// clang-format on
+
+template<typename T>
+constexpr T *impl(adl_tag, T *ptr, overload_priority<3>) noexcept
+{
+    static_assert(!std::is_function_v<T>, "STL prohibits function pointers in `to_address`");
+    return ptr;
+}
+
+} // namespace detail_to_address
+
+// clang-format off
+
+// `std::to_address` is not SFINAE-friendly and it doesn't support `{unique,shared}_ptr<T[]>`
+[[nodiscard]] constexpr auto to_address(const auto &ptr)
+    noexcept(noexcept(impl(detail_to_address::adl_tag{}, ptr, detail_to_address::highest_priority)))
+    -> decltype(impl(detail_to_address::adl_tag{}, ptr, detail_to_address::highest_priority))
+{
+    return impl(detail_to_address::adl_tag{}, ptr, detail_to_address::highest_priority);
+}
+// clang-format on
 
 } // namespace siga::util
