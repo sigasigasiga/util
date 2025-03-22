e3fc92e4c0e10071f58e7c18b9087efca04a2db7
Egor Bychin <e.siga@proton.me>
2025-03-23 03:14:34 +0400

    `make_bitset`: 3 times + construct `bitset` directly

--- include/siga/util/bitset.hpp
+++ include/siga/util/bitset.hpp
@@ -1,22 +1,15 @@
 #pragma once
 
 #include <bitset>
-#include <type_traits>
 
 namespace siga::util {
 
-template<std::convertible_to<bool>... Ts>
-[[nodiscard]] constexpr auto make_bitset(Ts &&...values)
-    noexcept((... && std::is_nothrow_convertible_v<Ts, bool>))
+template<typename... Args, std::size_t Sz = sizeof...(Args)>
+[[nodiscard]] constexpr auto make_bitset(Args &&...args)
+    noexcept(noexcept(std::bitset<Sz>{(char[]){(std::forward<Args>(args) ? '1' : '0')...}, Sz})) //
+    -> decltype(std::bitset<Sz>{(char[]){(std::forward<Args>(args) ? '1' : '0')...}, Sz})
 {
-    constexpr std::size_t count = sizeof...(Ts);
-    std::bitset<count> ret;
-
-    auto impl = [&]<std::size_t... Is>(std::index_sequence<Is...>) {
-        return (ret.set(Is, static_cast<bool>(values)), ...);
-    };
-
-    return impl(std::make_index_sequence<count>{});
+    return std::bitset<Sz>{(char[]){(std::forward<Args>(args) ? '1' : '0')...}, Sz};
 }
 
 } // namespace siga::util
