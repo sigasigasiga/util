1b8c59cc575909e1c1ede81a8026c6b51d10c76a
Egor Bychin <e.siga@proton.me>
2025-03-23 19:06:06 +0400

    add `reverse_arguments`

--- include/siga/util.hpp
+++ include/siga/util.hpp
@@ -9,6 +9,7 @@
 #include <siga/util/for_each_in_tuple.hpp>
 #include <siga/util/ignore.hpp>
 #include <siga/util/read_only_value.hpp>
+#include <siga/util/reverse_arguments.hpp>
 #include <siga/util/scoped.hpp>
 #include <siga/util/shared_from_this_base.hpp>
 #include <siga/util/storage_base.hpp>
--- include/siga/util/bitset.hpp
+++ include/siga/util/bitset.hpp
@@ -8,6 +8,7 @@ namespace siga::util {
 // clang-format off
 
 // the last one in the pack is the first one in the bitset, as in `bitset`'s constructor
+// you may use `util::reverse_arguments` to fix that
 template<typename... Args, std::size_t Sz = sizeof...(Args)>
 [[nodiscard]] constexpr auto make_bitset(Args &&...args)
     noexcept(noexcept(std::bitset<Sz>{std::array{(std::forward<Args>(args) ? '1' : '0')...}.data(), Sz}))
