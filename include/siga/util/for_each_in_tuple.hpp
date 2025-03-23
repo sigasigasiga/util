4160ec5fbe0b8db64e25ade0a7e2ce9d53fdae1a
Egor Bychin <e.siga@proton.me>
2025-03-23 05:21:00 +0400

    `for_each_in_tuple`: 3 times

--- include/siga/util/for_each_in_tuple.hpp
+++ include/siga/util/for_each_in_tuple.hpp
@@ -5,11 +5,15 @@
 namespace siga::util {
 
 template<typename F, typename Tuple>
-constexpr F for_each_in_tuple(F fn, Tuple &&tuple) //
+constexpr auto for_each_in_tuple(F fn, Tuple &&tuple) //
     noexcept(noexcept(std::apply(
         std::bind_front(fn::op::for_each_in_parameter_pack(), std::move(fn)),
         std::forward<Tuple>(tuple)
-    )))
+    ))) //
+    -> decltype(std::apply(
+        std::bind_front(fn::op::for_each_in_parameter_pack(), std::move(fn)),
+        std::forward<Tuple>(tuple)
+    ))
 {
     return std::apply(
         std::bind_front(fn::op::for_each_in_parameter_pack(), std::move(fn)),
