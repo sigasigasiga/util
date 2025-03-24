29f927cc92d06bebdcf4132ee206f43eb00c32f0
Egor Bychin <e.siga@proton.me>
2025-03-24 12:26:45 +0400

    make `lift_function` simpler

--- include/siga/fn/bind/lift_function.hpp
+++ include/siga/fn/bind/lift_function.hpp
@@ -4,25 +4,18 @@
 
 namespace sgia::fn::bind {
 
-namespace detail_lift_function {
-
-template<typename T, auto V>
-struct impl;
+// make a class from a function pointer that doesn't take any space and
+// correctly adds it to the overload set
+template<auto V>
+struct lift_function;
 
 template<typename Ret, typename... Args, bool Noexcept, Ret (*Fn)(Args...) noexcept(Noexcept)>
-struct impl<Ret (*)(Args...) noexcept(Noexcept), Fn>
+struct lift_function<Fn>
 {
     static constexpr Ret operator()(Args &&...args) noexcept(Noexcept)
     {
         return Fn(std::forward<Args>(args)...);
     }
 };
-
-} // namespace detail_lift_function
-
-// make a class from a function pointer that doesn't take any space and
-// correctly adds it to the overload set
-template<auto V>
-using lift_function = detail_lift_function::impl<decltype(V), V>;
 
 } // namespace sgia::fn::bind
