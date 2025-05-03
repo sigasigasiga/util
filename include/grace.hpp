412955f53b93d95852024d9bc267ec9e91707d68
Egor Bychin <e.siga@proton.me>
2025-05-03 23:46:53 +0400

    add `grace.hpp`

---
+++ include/grace.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <grace/compat.hpp>
+#include <grace/experimental.hpp>
+#include <grace/fn.hpp>
+#include <grace/iter.hpp>
+#include <grace/meta.hpp>
+#include <grace/ranges.hpp>
+#include <grace/toy.hpp>
+#include <grace/util.hpp>
--- test/include_all.cpp
+++ test/include_all.cpp
@@ -1,11 +1,4 @@
-#include <grace/compat.hpp>
-#include <grace/experimental.hpp>
-#include <grace/fn.hpp>
-#include <grace/iter.hpp>
-#include <grace/meta.hpp>
-#include <grace/ranges.hpp>
-#include <grace/toy.hpp>
-#include <grace/util.hpp>
+#include <grace.hpp>
 
 int main()
 {
