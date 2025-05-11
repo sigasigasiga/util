d59c5072d1909af79e8af8e2bb97208c574a8a27
Egor Bychin <e.siga@proton.me>
2025-05-11 18:29:45 +0400

    improve `lazy_eval`

--- include/grace/fn/bind/lazy_eval.hpp
+++ include/grace/fn/bind/lazy_eval.hpp
@@ -12,15 +12,23 @@ class [[nodiscard]] lazy_eval : private util::storage_base<F>
 public:
     using util::storage_base<F>::storage_base;
 
-public:
-    // TODO: you have to write it three times
-    template<typename Self, typename UF = meta::copy_cvref_t<Self &&, F>>
-    [[nodiscard]] constexpr operator std::invoke_result_t<UF>(this Self &&self)
-        noexcept(std::is_nothrow_invocable_v<UF>)
+private:
+    template<typename Self, typename USelf = meta::copy_cvref_t<Self &&, lazy_eval>>
+    static auto impl(Self &&self)
+        noexcept(noexcept(std::invoke(util::private_base_cast<USelf>(self).value())))
+        -> decltype(std::invoke(util::private_base_cast<USelf>(self).value()))
     {
-        using USelf = meta::copy_cvref_t<Self &&, lazy_eval>;
         return std::invoke(util::private_base_cast<USelf>(self).value());
     }
+
+public:
+    template<typename Self>
+    [[nodiscard]] constexpr operator decltype((impl)(std::declval<Self>()))(this Self &&self)
+        noexcept(noexcept((impl)(std::forward<Self>(self))))
+    {
+        return (impl)(std::forward<Self>(self));
+    }
+
 };
 
 template<typename F>
