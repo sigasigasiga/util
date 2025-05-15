836fb0156550cbb7ea1dc772ac4709e6562fde3a
Egor Bychin <e.siga@proton.me>
2025-05-16 00:49:02 +0400

    add `default_assign`

--- include/grace/util/utility.hpp
+++ include/grace/util/utility.hpp
@@ -104,4 +104,24 @@ requires std::is_convertible_v<From, To>
     return from;
 }
 
+// -------------------------------------------------------------------------------------------------
+
+// Generic way to implement assignment for any class that has
+// 1. `To(FwdFrom &&)` construcotr
+// 2. `.swap` method
+//
+// NB: I don't really like the idea of returning an lvalue reference
+//     but I don't like inconsistencies with the standard even more
+template<typename FwdTo, typename FwdFrom, typename To = std::remove_cvref_t<FwdTo>>
+FwdTo &default_assign(FwdTo &&to, FwdFrom &&from)
+    noexcept(noexcept(To(std::forward<FwdFrom>(from)).swap(to)))
+    requires requires { To(std::forward<FwdFrom>(from)).swap(to); }
+{
+    if(std::addressof(to) != std::addressof(from)) {
+        To(std::forward<FwdFrom>(from)).swap(to);
+    }
+
+    return to;
+}
+
 } // namespace grace::util
