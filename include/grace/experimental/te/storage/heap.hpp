145bc182ba6ed3099420af561fe5a46083f9e36b
Egor Bychin <e.siga@proton.me>
2025-06-03 16:12:46 +0400

    move `heap_storage` to `te::storage::heap`

--- include/grace/experimental/te/storage/heap.hpp
+++ include/grace/experimental/te/storage/heap.hpp
@@ -4,30 +4,30 @@
 
 #include <grace/util/utility.hpp>
 
-namespace grace::util {
+namespace grace::experimental::te::storage {
 
 // NB:
 // 1. It copies the underlying object on copy
 // 2. It is not usable in moved-from state even if the underlying object is
 template<typename T>
-class heap_storage
+class heap
 {
 public:
-    constexpr heap_storage() = default;
+    constexpr heap() = default;
 
-    constexpr heap_storage(const heap_storage &rhs)
+    constexpr heap(const heap &rhs)
         : clone_{rhs.clone_}
         , data_{clone_(*rhs.data_)}
     {
     }
 
-    constexpr heap_storage &operator=(const heap_storage &rhs)
+    constexpr heap &operator=(const heap &rhs)
     {
-        return (default_assign)(*this, rhs);
+        return util::default_assign(*this, rhs);
     }
 
-    constexpr heap_storage(heap_storage &&) = default;
-    constexpr heap_storage &operator=(heap_storage &&) = default;
+    constexpr heap(heap &&) = default;
+    constexpr heap &operator=(heap &&) = default;
 
     template<typename FwdU, typename U = std::remove_cvref_t<FwdU>>
     requires std::convertible_to<U *, T *>
@@ -41,13 +41,13 @@ public:
     requires std::convertible_to<std::remove_cvref_t<FwdU> *, T *>
     constexpr heap_storage &operator=(FwdU &&v)
     {
-        return (default_assign)(*this, std::forward<FwdU>(v));
+        return util::default_assign(*this, std::forward<FwdU>(v));
     }
 
     constexpr ~heap_storage() = default;
 
 public:
-    constexpr void swap(heap_storage &rhs) noexcept
+    constexpr void swap(heap &rhs) noexcept
     {
         std::swap(clone_, rhs.clone_);
         std::swap(data_, rhs.data_);
@@ -67,4 +67,4 @@ private:
     std::unique_ptr<T> data_;
 };
 
-} // namespace grace::util
+} // namespace grace::experimental::te::storage
